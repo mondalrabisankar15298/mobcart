f28f9b44b85fc93b15b128976242d7520dfa65e3
mondalrabisankar15298 <149944599+mondalrabisankar15298@users.noreply.github.com>
2025-01-20 10:30:49 +0530

    version 2

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <a class="logo" href="index.html">MobCart</a>
+                    <p>Affordable and Best Mobile </p>
+                </div>
+                <div class="footer-col-2">
+                    <h4>Help</h4>
+                    <ul>
+                        <li><a href="#">Plans</a></li>
+                        <li><a href="#">Track Order</a></li>
+                        <li><a href="#">Store Locator</a></li>
+                        <li><a href="#">Return Policy</a></li>
+                        <li><a href="#">My Account</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col-3">
+                    <h4>About Us</h4>
+                    <ul>
+                        <li><a href="#">Our Story</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Affiliate</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col-4">
+                    <h4>Follow Us</h4>
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Instgram</a></li>
+                        <li><a href="#">YouTube</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
